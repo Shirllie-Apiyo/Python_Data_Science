17b00df86b6a0896a03b13e1989ff52a7c442060
Shirllie-Apiyo <apraise320@gmail.com>
2021-12-11 01:25:08 +0300

    calculation

--- app.py
+++ app.py
@@ -4,3 +4,17 @@ print(7 + 10)
 
 # division
 print(5/7)
+
+# subtraction
+print(5-5)
+
+# Multiplication
+print(3*5)
+
+#  modulo (%)- this operator returns the remainder of the
+# division of the number to the left by the number on its right
+print(18 % 7)
+
+# exponentation: (**) - this operator raises the number to its left the power
+# of the number to its right
+print(6 ** 2)

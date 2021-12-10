230f2a2faec1c2fbcad258fc3fdd0d27ba842bf3
Shirllie-Apiyo <apraise320@gmail.com>
2021-12-11 02:22:27 +0300

    variables

---
+++ var.py
@@ -0,0 +1,17 @@
+# Suppose you have $100, which you can invest with a 10% return each year. After one year, it's 100 * 1.1 = 110 dollars,
+# and after two years it's 100*1.1*1.1 = 121.
+# Add code to calculate how much money you end up with after 7 years, and print the result.
+
+
+# Create a variable savings
+savings = 100
+
+# Create a variable growth_multiplier
+growth_multiplier = 1.1
+
+# Calculate result
+result = 100 * 1.1**7
+
+
+# Print out result
+print(result)

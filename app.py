5a89c505a9b36873a0dd9e2f259b53c85eb28569
Shirllie-Apiyo <apraise320@gmail.com>
2021-12-11 01:47:39 +0300

    calculation

--- app.py
+++ app.py
@@ -18,3 +18,8 @@ print(18 % 7)
 # exponentation: (**) - this operator raises the number to its left the power
 # of the number to its right
 print(6 ** 2)
+
+# Suppose you have $100, which you can invest with a 10% return each year. After one year, it's 100 * 1.1 = 110 dollars,
+# and after two years it's 100*1.1*1.1 = 121.
+# Add code to calculate how much money you end up with after 7 years, and print the result.
+print(100 * 1.1**7)
